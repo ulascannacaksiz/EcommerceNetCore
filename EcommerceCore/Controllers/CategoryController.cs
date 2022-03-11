58ce1d3f373eeb96a66f779288b96f3e231478b1
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-03-11 12:37:29 +0300

    filitreleme işlemi düzeltildi

--- EcommerceCore/Controllers/CategoryController.cs
+++ EcommerceCore/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Linq;
 
 namespace EcommerceCore.Controllers
@@ -18,14 +19,19 @@ namespace EcommerceCore.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(int id, string brands)
+        public IActionResult Index(int id, string[] brands)
         {
             //decimal minPrice, decimal? maxPrice, string[] colors,
             ProductManager pm = new ProductManager(new EfProductRepository());
             var values = pm.GetProductAllWithImage().Where(p => p.CategoryId == id).Where(p => brands.Contains(p.Brand)).ToList();
-            var brands1 = values.Where(p => brands.Contains(p.Brand));
-            ViewBag.Brands = brands1;
-            return View(values);
+
+            var result=  JsonConvert.SerializeObject(values, Formatting.Indented,
+            new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            });
+            return Json(result);
+
         }
     }
 }
